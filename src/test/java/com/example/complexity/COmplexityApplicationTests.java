b5616c3bfb2c0c237c8b62284f58d614de5f59c3
Matěj Kala <kalamatej@gmail.com>
2023-12-28 20:13:34 +0100

    Init

---
+++ src/test/java/com/example/complexity/COmplexityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.complexity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class COmplexityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
